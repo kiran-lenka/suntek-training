1556bf889f9f02790814fbfc3a71f574d24126c8
Kiran Lenka <79368502+kiran-lenka@users.noreply.github.com>
2021-02-21 22:39:28 +0530

    Update Day1 - searchInRotatedSortedArray.py

--- Day1 - searchInRotatedSortedArray.py
+++ Day1 - searchInRotatedSortedArray.py
@@ -1,11 +1,40 @@
 class Solution:
-
-# Bruteforce O(n) 
     def search(self, nums: List[int], target: int) -> int:
+        ''' 
+        #Bruteforce O(n)
         index = -1
         for i in range(len(nums)):
             if nums[i] == target:
                 index = i
                 break
-        return index
-                
+        '''
+        #Follow up for achieving O(log n)
+        
+        left = 0
+        right = len(nums)-1
+        
+        while (left<right):
+            midpoint = left + int((right-left)/2)
+            if(nums[midpoint]>nums[right]):
+                left = midpoint+1
+            else:
+                right = midpoint
+        
+        start = left
+        left = 0
+        right = len(nums)-1
+        
+        if (target>=nums[start] and target<= nums[right]):
+            left = start
+        else:
+            right = start
+            
+        while (left<= right):
+            midpoint = left + int((right-left)/2)
+            if(nums[midpoint]==target):
+                return midpoint
+            elif (nums[midpoint]<target):
+                left = midpoint+1
+            else:
+                right = midpoint-1 
+        return -1
